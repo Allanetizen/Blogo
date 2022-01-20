6e294468c322384dd96fadfffcefc2be4459643e
Allanetizen <allankosgei747@gmail.com>
2022-01-20 16:49:59 +0300

    Navbar Compnents and Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = 'Welcome To Blogo';
-  const titleTwo ='This is the second title'
+  const title = "Welcome To Blogo";
+  const titleTwo = "This is the second title";
   //NB: objects cannot be used as variables
   const likes = 1000;
-  const githubLink ="https://github.com/allanetizen"
+  const githubLink = "https://github.com/allanetizen";
   return (
-    <div className="App">
+    <div className="App"> 
+      <Navbar />
       <div className="content">
-        <h1>{title},<br/> {titleTwo}</h1>
-        <p>Your have <strong>{likes }</strong> likes</p>
-        <p> {Math.random()* 10}</p>
-        <p>My GitHub Link is <a href={githubLink}>here</a></p>
-
-
-      </div>
+        <Home/>
       
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (  
+        <div className="home">
+            <h2>HomePage </h2>
+
+        </div>
+
+        
+    );
+}
+ 
+export default Home;
