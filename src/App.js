262d43cb60bc79cf2c309c989a5d62a464707a39
Allanetizen <allankosgei747@gmail.com>
2022-01-20 15:12:26 +0300

    covered components, templates and dynamic content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const title = 'Welcome To Blogo';
+  const titleTwo ='This is the second title'
+  //NB: objects cannot be used as variables
+  const likes = 1000;
+  const githubLink ="https://github.com/allanetizen"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title},<br/> {titleTwo}</h1>
+        <p>Your have <strong>{likes }</strong> likes</p>
+        <p> {Math.random()* 10}</p>
+        <p>My GitHub Link is <a href={githubLink}>here</a></p>
+
+
+      </div>
+      
     </div>
   );
 }
