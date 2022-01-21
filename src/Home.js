f650f5ba891a11ecb957ecec3bf9de05a22f6c44
Allanetizen <allankosgei747@gmail.com>
2022-01-21 16:49:21 +0300

    events and anonymous functions

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 const Home = () => {
+
+    const handleClick= (e) => {
+        console.log('hello dev',e)
+    }
+    const clickTwo = (name,e) => {
+        console.log(e.target,'hello '+ name);
+    }
     return (  
         <div className="home">
             <h2>HomePage </h2>
+            <button onClick={handleClick}>Click me</button>
+         
+            <button onClick={(e)=> clickTwo('Dev',e)
+            }>Click2</button>
 
         </div>
 
-        
+         
     );
 }
  
