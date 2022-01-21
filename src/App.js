80acac465951a2af0ff4de0caf61b241a7a82519
Allanetizen <allankosgei747@gmail.com>
2022-01-21 21:55:30 +0300

    SetState component2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome To Blogo";
-  const titleTwo = "This is the second title";
+  
   //NB: objects cannot be used as variables
-  const likes = 1000;
-  const githubLink = "https://github.com/allanetizen";
+  
   return (
     <div className="App"> 
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,24 @@ import { useState } from "react";
 const Home = () => {
     //let  name = 'Allan';
     const [name, setName]= useState('Kibet');
+    const[age, setAge] = useState(12);
 
     const handleClick = () => {
         setName('Allan'); 
+        
+    }
+    const clickTwo = () => {
+     setAge(20);
     }
    
     
     return (  
         <div className="home">
             <h2>HomePage </h2>
-            <p>{name}</p>
+            <p>{name} is <b>{age}</b> Years Old</p>
+            
+            <button onClick={clickTwo}>ChangeAge</button>
+
             <button onClick={handleClick}>Click me</button>
            
          
