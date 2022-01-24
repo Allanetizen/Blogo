35ae6bb2e329d7f6d0bca5766d23d577b0a15e16
Allanetizen <allankosgei747@gmail.com>
2022-01-24 13:40:47 +0300

    reusing components,

--- src/BlogList.js
+++ src/BlogList.js
@@ -10,6 +10,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
+          
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import BlogList from "./BlogList";
 const Home = () => {
     
     const[ blogs]= useState([
-        {title: 'Healthy Eating', body:'Lorem ipsum....', author:'Allanetizen', id:1},
-        {title: 'Tech Bits', body:'Lorem ipsum....', author:'Allanetizen', id:2},
-        {title: 'Farmers Choice', body:'Lorem ipsum....', author:'Allanetizen', id:3}
+        {title: 'Healthy Eating', body:'Lorem ipsum....', author:'Tony Montana', id:1},
+        {title: 'Tech Bits', body:'Lorem ipsum....', author:'Timmy Turner', id:2},
+        {title: 'Farmers Choice', body:'Lorem ipsum....', author:'Tony Montana', id:3}
 
     ]);
    
@@ -15,6 +15,7 @@ const Home = () => {
         <div className="home">
             {/*Bloggy  passes blog as a prop*/}
             <BlogList blogs={blogs} title="All blogs!"/>
+            <BlogList blogs={blogs.filter((blog)=> blog.author ==='Tony Montana')} title="Tony's blogs!"/>
            
              
           
