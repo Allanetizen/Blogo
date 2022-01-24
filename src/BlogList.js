0187e3f30b8882115bb9f6f223dfdf30e06d4a5e
Allanetizen <allankosgei747@gmail.com>
2022-01-24 14:38:25 +0300

    using functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
     //const blogs = props.blogs;
     //const title =props.title;
 
@@ -10,6 +10,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
+          <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
           
         </div>
       ))}
--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     
-    const[ blogs]= useState([
+    const[ blogs, setBlogs]= useState([
         {title: 'Healthy Eating', body:'Lorem ipsum....', author:'Tony Montana', id:1},
         {title: 'Tech Bits', body:'Lorem ipsum....', author:'Timmy Turner', id:2},
         {title: 'Farmers Choice', body:'Lorem ipsum....', author:'Tony Montana', id:3}
 
     ]);
+    //function for deleting
+    const handleDelete= (id)=>{
+        const newBlogs = blogs.filter(blog=>blog.id != id);
+        setBlogs(newBlogs);
+
+    }
    
     
     return (  
         <div className="home">
-            {/*Bloggy  passes blog as a prop*/}
-            <BlogList blogs={blogs} title="All blogs!"/>
-            <BlogList blogs={blogs.filter((blog)=> blog.author ==='Tony Montana')} title="Tony's blogs!"/>
+            {/*Blogs  passes blog as a prop*/}
+                    
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
            
              
           
