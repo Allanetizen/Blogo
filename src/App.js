d8d36c2b23c437f571a1383a0eeab6adcd8d9f1f
Allanetizen <allankosgei747@gmail.com>
2022-02-02 17:18:38 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   
   //NB: objects cannot be used as variables
   
   return (
-    <div className="App"> 
+    <Router>
+      <div className="App"> 
       <Navbar />
       <div className="content">
-        <Home/>
+        <Switch>
+          {/* makes sure only one route shows at any given time */}
+          <Route path="/">
+            <Home/>
+
+          </Route>
+        </Switch>
+        
       
       </div>
     </div>
+    </Router>
+    
   );
 }
 
