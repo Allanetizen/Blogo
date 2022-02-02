8bdd1a74cc144c2563692f8929513e65456d875e
Allanetizen <allankosgei747@gmail.com>
2022-02-02 21:13:39 +0300

    exact match routes and routes ;)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
-  
   //NB: objects cannot be used as variables
-  
+
   return (
     <Router>
-      <div className="App"> 
-      <Navbar />
-      <div className="content">
-        <Switch>
-          {/* makes sure only one route shows at any given time */}
-          <Route path="/">
-            <Home/>
-
-          </Route>
-        </Switch>
-        
-      
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            {/* makes sure only one route shows at any given time */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
-    
   );
 }
 
