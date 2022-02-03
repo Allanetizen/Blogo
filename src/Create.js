5ca7f9e46ae7ea408eb5aee081ff9f0dd06ea2c2
Allanetizen <allankosgei747@gmail.com>
2022-02-03 23:38:52 +0300

    submit events

--- src/Create.js
+++ src/Create.js
@@ -1,29 +1,42 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [title, setTitle]= useState('');
-    const [body, setBody]= useState('');
-    const [author, setAuthor]= useState('Allan');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Allan");
+  const handleSubmit = (e) => {
+    //prevents page from reloading/refresh
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
-        <input type="text" required value={title} onChange={(e)=> setTitle(e.target.value)}></input>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
         <label>Blog body:</label>
-        <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog Author:</label>
-        <select  value={author} onChange={(e)=>setAuthor(e.target.value)}>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="Allan">Allan</option>
           <option value="Tommy">Tommy</option>
           <option value="Tommy">Turner</option>
-          
         </select>
         <button>Add Blog</button>
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
-
       </form>
     </div>
   );
