8c2112595301cf60810d2211ca09331dd73739cb
Allanetizen <allankosgei747@gmail.com>
2022-02-04 01:08:49 +0300

    pragmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,54 +1,55 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("Allan");
-  const [isPending, setIspending] = useState('false');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const history = useHistory();
+
   const handleSubmit = (e) => {
-    //prevents page from reloading/refresh
     e.preventDefault();
     const blog = { title, body, author };
-    fetch("http://localhost:5000/blogs", {
-      method: "POST",
+
+    fetch('http://localhost:5000/blogs/', {
+      method: 'POST',
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(blog),
+      body: JSON.stringify(blog)
     }).then(() => {
-      console.log("new blog added successfully!!");
-    });
-  };
+      // history.go(-1);
+      history.push('/');
+    })
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
-        <label>Blog Title:</label>
-        <input
-          type="text"
-          required
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-        ></input>
+        />
         <label>Blog body:</label>
         <textarea
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        <label>Blog Author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="Allan">Allan</option>
-          <option value="Tommy">Tommy</option>
-          <option value="Tommy">Turner</option>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="SwaySway">SwaySway</option>
+          <option value="Badus">Badus</option>
         </select>
-       { !isPending&& <button>Add Blog</button>}
-       { isPending&& <button disabled>Adding Blog . . .</button>}
-
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        <button>Add Blog</button>
       </form>
     </div>
   );
-};
-
+}
+ 
 export default Create;
