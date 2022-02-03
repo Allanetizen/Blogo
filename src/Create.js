d2b16337b5e219da2ce0d96f10f390fc9e7ba41d
Allanetizen <allankosgei747@gmail.com>
2022-02-03 22:39:03 +0300

    controlled input forms

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,22 @@
 const Create = () => {
-    return (
-        <div className="create">
-            <h2>Add a New Blog</h2>
-        </div>
-      );
-}
- 
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required></input>
+        <label>Blog body:</label>
+        <textarea required></textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="Allan">Allan</option>
+          <option value="Tommy">Tommy</option>
+          
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
