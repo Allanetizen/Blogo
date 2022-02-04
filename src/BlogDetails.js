dcd301a6ec3b0809f9fa4db3d89790f952477a8e
Allanetizen <allankosgei747@gmail.com>
2022-02-04 13:32:56 +0300

    deleting blogs using the DELETE METHOD

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import UseFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: blog, error, isPending } = UseFetch(
-    "http://localhost:5000/blogs/" + id);
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = UseFetch("http://localhost:5000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:5000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+    console.log("deleted successfully...");
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -13,8 +26,10 @@ const BlogDetails = () => {
         <article>
           <h2>{blog.title}</h2>
           <div>{blog.body}</div>
-          <p>Written by: <strong>{blog.author}</strong></p>
-          
+          <p>
+            Written by: <strong>{blog.author}</strong>
+          </p>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
