6cb923a3e8605eab1a18b366d7d713c6b2bd2d19
Allanetizen <allankosgei747@gmail.com>
2022-02-18 15:21:12 +0300

    added command for starting json server

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 //sfc - stateless functional component
 
 
+
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1> <Link to="/">The Blogo Blog</Link></h1>
+      <h1> <Link to="/">The Blogo Blog</Link></h1> 
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
